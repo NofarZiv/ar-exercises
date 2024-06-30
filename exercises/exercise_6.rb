8c3323d6c6ebe244d711e5011200a6eb2faa40f3
NofarZiv <nofarziv13@gmail.com>
2024-06-30 10:47:19 -0230

    create a few new employees.

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -5,7 +5,20 @@ require_relative './exercise_3'
 require_relative './exercise_4'
 require_relative './exercise_5'
 
+
 puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+
+@store1.employees.create(first_name: "Ariel", last_name: "Arnon", hourly_rate: 55)
+
+@store1.employees.create(first_name: "Nicole", last_name: "Elon", hourly_rate: 40)
+
+@store2.employees.create(first_name: "Haim", last_name: "Hamam", hourly_rate: 70)
+
+@store2.employees.create(first_name: "Trevor", last_name: "Sull", hourly_rate: 60)
+
+@store2.employees.create(first_name: "Daniel", last_name: "Ghust", hourly_rate: 65)
